e69afe83cb7ea3c38257d6b8a7fc5f25b2062369
CrisDgrnu <cris.dgrnu@gmail.com>
2022-06-02 18:15:38 +0200

    Add e2e tests for concepts find methods

--- backend/src/test/java/com/example/backend/api/core/concept/e2e/ConceptE2ETest.java
+++ backend/src/test/java/com/example/backend/api/core/concept/e2e/ConceptE2ETest.java
@@ -3,6 +3,7 @@ package com.example.backend.api.core.concept.e2e;
 import com.example.backend.api.core.concept.dto.ConceptDTO;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,13 +13,17 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class ConceptE2ETest {
 
     String BASE_URL = "/concepts/";
 
-    ConceptDTO basicConceptDTO = new ConceptDTO("Software", new LinkedList<>());
+    ConceptDTO conceptDTO1 = new ConceptDTO("Software", new LinkedList<>());
+    ConceptDTO conceptDTO2 = new ConceptDTO("Hardware", new LinkedList<>());
+    ConceptDTO conceptDTO3 = new ConceptDTO("Functional Programming", new LinkedList<>());
+    ConceptDTO conceptDTO4 = new ConceptDTO("Unix", new LinkedList<>());
+    ConceptDTO conceptDTO5 = new ConceptDTO("Haskell", new LinkedList<>());
 
     ConceptDTO wrongConceptDTO = new ConceptDTO("", new LinkedList<>());
 
@@ -29,12 +34,12 @@ public class ConceptE2ETest {
     }
 
     @Test
-    @DisplayName("should create a Concept giving a basic DTO")
+    @DisplayName("Should create a Concept giving a basic DTO")
     void createWithBasicDTO(){
 
         given()
                 .contentType("application/json")
-                .body(basicConceptDTO)
+                .body(conceptDTO1)
         .when()
                 .post(BASE_URL)
         .then()
@@ -45,7 +50,7 @@ public class ConceptE2ETest {
     }
 
     @Test
-    @DisplayName("should not create a Concept giving a wrong DTO")
+    @DisplayName("Should not create a Concept giving a wrong DTO")
     void createWithWrongDTO(){
         given()
                 .contentType("application/json")
@@ -57,5 +62,68 @@ public class ConceptE2ETest {
                 .contentType(ContentType.JSON)
                 .body("message", res -> equalTo("Field text in DTO is mandatory"))
                 .body("httpStatus", res -> equalTo("BAD_REQUEST"));
+    }
+
+    @Test
+    @DisplayName("Should find a Concept with the given id")
+    void findOneWithAnExistingId(){
+        int id = createConcept(conceptDTO1).extract().path("id");
+
+        given()
+                .accept(ContentType.JSON)
+                .pathParam("id", id)
+        .when()
+                .get(BASE_URL+"{id}")
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("text", res -> equalTo("Software"))
+                .body("answers", res -> equalTo(Collections.emptyList()))
+                .body("_links", res -> hasKey("self"))
+                .body("_links", res -> hasKey("concepts"));
+    }
+
+    @Test
+    @DisplayName("Should not find a Concept with the given id")
+    void findOneWithANonExistingId(){
+        int id = 999;
+
+        given()
+                .accept(ContentType.JSON)
+                .pathParam("id", id)
+        .when()
+                .get(BASE_URL+"{id}")
+        .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    @DisplayName("Should find a Page of Concepts")
+    void findAllWithConceptsInDatabase(){
+        createConcept(conceptDTO1);
+        createConcept(conceptDTO2);
+        createConcept(conceptDTO3);
+        createConcept(conceptDTO4);
+        createConcept(conceptDTO5);
+
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get(BASE_URL+"?page=0")
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("_embedded",res -> hasKey("conceptList"))
+                .body("_embedded.conceptList.size()",res -> is(5))
+                .body("_links", res -> hasKey("prev"))
+                .body("_links", res -> hasKey("next"));
+    }
+
+    //TODO Create FindAll test when the database is empty (DELETE method needed)
+
+    private ValidatableResponse createConcept(ConceptDTO conceptDTO) {
+        return given()
+                .contentType("application/json")
+                .body(conceptDTO)
+                .when()
+                .post(BASE_URL).then();
     }
 }
--- backend/src/test/java/com/example/backend/api/core/concept/service/ConceptServiceTest.java
+++ backend/src/test/java/com/example/backend/api/core/concept/service/ConceptServiceTest.java
@@ -14,8 +14,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -27,36 +33,72 @@ import static org.mockito.Mockito.lenient;
 class ConceptServiceTest {
 
     @Mock
-    ConceptRepository conceptRepository;
+    private ConceptRepository conceptRepository;
 
     @Mock
-    AnswerRepository answerRepository;
+    private AnswerRepository answerRepository;
 
     @InjectMocks
-    ConceptService conceptService;
+    private ConceptService conceptService;
 
-    Concept concept = new Concept(1L, "Software", new LinkedList<>());
+    private final Concept concept1 = new Concept(1L, "Software", new LinkedList<>());
+    private final Concept concept2 = new Concept(2L, "Hardware", new LinkedList<>());
+    private final Concept concept3 = new Concept(3L, "Functional Programming", new LinkedList<>());
+
+    private final ConceptDTO conceptDTO1 = new ConceptDTO("Software", new LinkedList<>());
+    private final ConceptDTO wrongConceptDTO = new ConceptDTO();
+
+    private final Page<Concept> conceptPage = new PageImpl(List.of(concept1, concept2, concept3));
 
     @BeforeEach
     void setUpMocks() {
-        lenient().when(conceptRepository.save(any(Concept.class))).thenReturn(concept);
+        lenient().when(conceptRepository.save(any(Concept.class))).thenReturn(concept1);
+
+        lenient().when(conceptRepository.findById(1L)).thenReturn(Optional.of(concept1));
+        lenient().when(conceptRepository.findById(2L)).thenReturn(Optional.empty());
+
+        lenient().when(conceptRepository.findAll(PageRequest.of(0, 5))).thenReturn(conceptPage);
+        lenient().when(conceptRepository.findAll(PageRequest.of(1, 5))).thenReturn(new PageImpl<>(List.of()));
     }
 
     @Test
     @DisplayName("Should create a Concept with a correct DTO")
     void createWithCorrectDTO() {
-        ConceptDTO conceptDTO = new ConceptDTO("Software", new LinkedList<>());
+        Concept createdConcept = conceptService.create(conceptDTO1);
 
-        Concept createdConcept = conceptService.create(conceptDTO);
-
-        assertEquals(concept, createdConcept);
+        assertEquals(concept1, createdConcept);
     }
 
     @Test
     @DisplayName("Should not create a Concept with an incorrect DTO")
     void createWithWrongDTO(){
-        ConceptDTO wrongConceptDTO = new ConceptDTO();
-
         assertThrows(DTOBadRequestException.class, () -> conceptService.create(wrongConceptDTO));
+    }
+
+    @Test
+    @DisplayName("Should find one Concept with the given id")
+    void findOneThatExist(){
+        Concept concept = conceptService.findOne(1L);
+        assertEquals(concept,concept1);
+    }
+
+    @Test
+    @DisplayName("Should throw an Exception with the given id")
+    void findOneThatNotExist(){
+        assertThrows(EntityNotFoundException.class, () -> conceptService.findOne(2L));
+    }
+
+    @Test
+    @DisplayName("Should find a page with 3 Concepts")
+    void findAllWithData(){
+        Page<Concept> conceptPage = conceptService.findAll(0);
+        assertEquals(conceptPage.getNumberOfElements(),3);
+    }
+
+    @Test
+    @DisplayName("Should find an empty Page")
+    void findAllWithNoData(){
+        Page<Concept> conceptPage = conceptService.findAll(1);
+        assertEquals(conceptPage.getNumberOfElements(),0);
     }
 }
